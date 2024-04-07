2c069b45c4a91729b50cffd6f1abcd43cdb6187f
neifau <uneifa@gmail.com>
2024-04-07 21:06:00 +0700

    uplaod file

---
+++ App/Model/akun_user.php
@@ -0,0 +1,22 @@
+<?php
+
+Class akun_user{
+    public $id;
+    public $name;
+    public $email;
+    public $password;
+
+    static function select(){
+
+    }
+    static function insert(){
+
+    }
+    static function update(){
+
+    }
+    static function delete(){
+
+    }
+    
+}
---
+++ App/Model/data_owner.php
@@ -0,0 +1,21 @@
+<?php
+
+Class data_owner{
+    public $id_owner;
+    public $name_owner;
+    public $phone_owner;
+
+    static function select(){
+
+    }
+    static function insert(){
+
+    }
+    static function update(){
+
+    }
+    static function delete(){
+
+    }
+    
+}
